291d0978805214dd4b0d65f35900640fc9cf1d84
Ruben Vincenten <rubenvincenten@gmail.com>
2013-07-06 13:31:44 +0200

    typo fix, add pico, add plugins dir

--- index.php
+++ index.php
@@ -5,8 +5,14 @@ setlocale(LC_ALL, 'nl_NL.utf-8', 'nl.utf-8', 'nl_NL', 'nl', 'dutch', 'nld');
 
 define('ROOT_DIR', realpath(dirname(__FILE__)) . '/');
 define('CONTENT_DIR', ROOT_DIR . 'content/');
-define('CONTENT_EXR', '.md');
+define('CONTENT_EXT', '.md');
 
+define('PLUGINS_DIR', ROOT_DIR . 'plugins/');
 define('MODULES_DIR', ROOT_DIR . 'modules/');
 
 define('PICO_DIR', MODULES_DIR . 'pico/');
+
+require PICO_DIR . 'vendor/autoload.php';
+require PICO_DIR . 'lib/pico.php';
+
+new Pico();
